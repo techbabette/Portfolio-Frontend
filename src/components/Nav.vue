5905f9dd520bb6b7b1258c93e96c0d9d7d89e084
techbabette <marinakrsticrf@gmail.com>
2023-06-29 14:23:30 +0200

    Landing page styling

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,9 @@
 <template>
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg border-bottom">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Name Lastname</a>
+    <router-link class="navbar-brand" to="/">
+      Name Lastname
+    </router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
--- src/components/pages/Landing.vue
+++ src/components/pages/Landing.vue
@@ -1,10 +1,12 @@
 <template>
 <div class="ViewHeight" id="landingBody">
-    <div data-aos="zoom-in-down" style="text-align: center;">
-        <h1 id="AuthorName">Name Lastname</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus delectus quam deserunt natus suscipit iusto amet voluptate cum, vitae nisi!</p>
+    <div data-aos="zoom-in-down" class="text-center row justify-content-center" style="text-align: center;">
+        <div class="col-8" style="border: 1px solid #292929; border-radius: 5%;">
+            <h1 id="AuthorName" class="my-4">Name Lastname</h1>
+            <p>PHP Developer Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, iste quos! Quis aperiam perspiciatis quam in fugit atque voluptates delectus?</p>
+           <router-link to="/projects" class="btn PrimaryButton my-4">View my projects</router-link>
+        </div>
     </div>
-    <router-link to="/projects" class="btn PrimaryButton">View my projects</router-link>
 </div>
 </template>
 <script>
