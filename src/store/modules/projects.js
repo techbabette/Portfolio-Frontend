6eaa69837f51cecfd6f15565dfd6f8045d0c3b82
techbabette <marinakrsticrf@gmail.com>
2024-01-14 10:47:58 +0100

    Switch to S3

--- src/store/modules/projects.js
+++ src/store/modules/projects.js
@@ -29,7 +29,7 @@ export default {
     actions : {
         async getProjectsFromApi({commit}){
             try{
-                let response = await axios.get("https://8udxt8e7t5.execute-api.eu-north-1.amazonaws.com/projects");
+                let response = await axios.get("https://tbabetteprojectsbucket.s3.eu-north-1.amazonaws.com/projects");
                 commit("setProjects", response.data);
             }
             catch(err){
